0d5ebf357e518cfeabbe51061d0f39344977455a
Szilágyi László <raptile666@gmail.com>
2022-01-29 14:49:01 +0100

    app.js szerkesztése kész (fetch + megjelenítés), html és css finomhangolás

---
+++ app.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById("btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", async () => {
+
+    const res = await fetch("cars.json");
+    const data = await res.json();
+
+    console.log(data);
+
+    let output = "";
+
+    data.forEach( car => {
+        output += 
+            `<div class="car">
+                <h3>${car.brand}</h3>
+                <h5>${car.type}</h5>
+                <h5>${car.factoryYear}</h5>
+            </div>`;
+    });
+
+    result.innerHTML = output;
+})
